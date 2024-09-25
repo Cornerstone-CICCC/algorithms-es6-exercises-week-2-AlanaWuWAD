ade94792fc1b7dadde46e49d5d26739e71b41aac
Alana <rcclewu@gmail.com>
2024-09-24 17:14:40 -0700

    completed

--- ex03.js
+++ ex03.js
@@ -8,11 +8,29 @@ Create a function named numberOfVowels that will receive a string and return the
 */
 
 const numberOfVowels = function (data) {
-  // Put your solution here
-};
+  let num = 0
+  const vowels = ['a','e','i','o','u']
+  let arr = data.split('')
+  arr.forEach(word => {
+    vowels.forEach(item => word === item? ++num:num)})
+  return num
+}
 
 console.log(numberOfVowels("orange")); // 3
 console.log(numberOfVowels("cornerstone college")); // 7
 console.log(numberOfVowels("aeiou")); // 5
 
 module.exports = numberOfVowels;
+
+
+// const numberOfVowels = function (data) {
+//   let num = 0
+//   let arr = data.split('')
+//   arr.forEach(word => {
+//     if (word === 'a' || word === 'e' || word === 'i' || word === 'o' || word === 'u') {
+//       num += 1
+//     }
+//   })
+//   return num
+// };
+
