02e6a62e2e7c13a86c12a6f432a8611b320959d4
Alana <rcclewu@gmail.com>
2024-09-29 21:49:44 -0700

    completed

--- ex19.js
+++ ex19.js
@@ -66,3 +66,58 @@ Instruction
 Create a function generateBoard which will return a nested array representing the board, containing the location of two queens.
 Create a function called queenThreat that will indicate whether or not the two queens are positioned so that they attack each other.
 */
+
+function generateBoard(whiteQueen, blackQueen) {
+  let chess = new Array()
+  for (let i=0; i<8; i++) {
+    chess[i] = new Array()
+    for (let j=0; j<8; j++) {
+      chess[i][j] = 0
+    }
+  }
+  chess[whiteQueen[0]][whiteQueen[1]]= 1
+  chess[blackQueen[0]][blackQueen[1]]= 1
+  return chess
+}
+
+function queenThreat(board){
+  let chessPosition = []
+  board.forEach((num,i) => {
+    let position = num.indexOf(1)
+    if(position >= 0){
+      if(chessPosition == []){
+        chessPosition = [i,chessPosition]}
+      else {
+        chessPosition.push([i,position])
+      }
+    }  
+  });
+
+  let check = {sum:[], subtract:[]}
+  chessPosition.forEach(num => {
+    check.sum.push(num[0]+num[1])
+    check.subtract.push(Math.abs(num[0]-num[1]))
+  })
+  
+  if(check.sum[0] === check.sum[1]){
+    return true
+  }else if(check.subtract[0] === check.subtract[1]){
+    return true
+  }else if(chessPosition[0][0] === chessPosition[1][0] || chessPosition[1][0] === chessPosition[1][1]){
+    return true
+  }else {
+    return false
+  }
+}
+
+let whiteQueen = [0, 5];
+let blackQueen = [5, 0];
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
+
+whiteQueen = [0, 0];
+blackQueen = [5, 7];
+generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
